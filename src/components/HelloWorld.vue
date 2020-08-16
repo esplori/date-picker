2cb54c6bfdc98d5c68d5fd559e894d7ce306d3bf
vistol <2381773226@qq.com>
2020-08-16 16:56:50 +0800

    add picker style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="date-picker">
+    <div>
+      <div class="year-month"><span>&lt;&lt;</span> <span @click="monthChangeDown">&lt;</span> {{currentYear}} 年{{currentMonth}} 月 <span>&gt;</span> <span>&gt;&gt;</span></div>
+      <ul class="days-container">
+        <li v-for="(item, index) in dayList" :key="index" class="single-day">{{item}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      dayList: [],
+      currentYear: new Date().getFullYear(),
+      currentMonth: new Date().getMonth() + 1
+    }
+  },
+  mounted () {
+    this.randerDayList()
+  },
+  methods: {
+    monthChangeDown () {
+      debugger
+      this.currentMonth = --this.currentMonth
+      this.randerDayList()
+    },
+    randerDayList () {
+      let days = this.getCurrentMonthDays(this.currentYear, this.currentMonth)
+      this.dayList = this.initDayList(days)
+    },
+    initDayList (days) {
+      days = days || 0
+      let list = []
+      for (let i = 1; i <= days; i++) {
+        list.push(i)
+      }
+      console.log(list)
+      return list
+    },
+    getCurrentMonthDays (inputYear, inputMonth) {
+      let currentDate = new Date()
+      let year = inputYear || currentDate.getFullYear()
+      let month = inputMonth || currentDate.getMonth() + 1 // 默认从0开始，所以需要加1
+      let isRn = false
+      let days = 0
+      if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        // 判断是否为闰年：能被400整除或者能被4整除，但不能被100整除
+        isRn = true
+      } else {
+        isRn = false
+      }
+      if (
+        month === 1 ||
+        month === 3 ||
+        month === 5 ||
+        month === 7 ||
+        month === 8 ||
+        month === 10 ||
+        month === 12
+      ) {
+        // 1、3、5、7、8、10、12 每月31天
+        days = 31
+      } else if (month === 2) {
+        if (isRn) {
+          // 为闰年，则多一天为29天
+          days = 29
+        } else {
+          days = 28
+        }
+      } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+        // 4、6、9、10、11
+        days = 30
+      }
+      return days
     }
   }
 }
@@ -96,18 +82,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.date-picker{
+  width: 50%;
+  margin: 0 auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.days-container{
+  width:370px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  text-align: left;
 }
-li {
+.single-day{
   display: inline-block;
-  margin: 0 10px;
+  width: 29px;
+  padding: 11px;
 }
-a {
-  color: #42b983;
+.year-month{
+  width:370px;
+  text-align: center;
 }
-</style>
+</style>>
